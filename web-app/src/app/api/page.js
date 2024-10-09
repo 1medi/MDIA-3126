d54e5cd2754344f4423021b0852c3d8f58eaee39
1medi <144403286+1medi@users.noreply.github.com>
2024-10-09 13:41:35 -0700

    fetchhhh

--- web-app/src/app/api/page.js
+++ web-app/src/app/api/page.js
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 /** Create a product page
  * create a header
@@ -9,12 +11,33 @@ import Image from "next/image";
  */
 
 export default function ProductPage() {
+
+  const [products, setProducts] = useState(null);
+  const API_ENDPOINT = "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+
+  async function fetchProducts() {
+    const response = await fetch(API_ENDPOINT);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setProducts();
+  }
+
+  const ProductList = () => {
+    return (
+      <div className="border-4 border-white p-4 mt-4">
+        Products will go here
+      </div>
+    )
+  }
+
   return (
     <div className="bg-emerald-500">
       <header className="border-4 border-white p-4 mt-4">
         <h1 className="text-7xl mt-4 mb-4">Welcome to my product page!</h1>
-        <button className="rounded-md bg-black p-4">Fetch Products</button>
+        <button onClick={fetchProducts} className="rounded-md bg-black p-4">Fetch Products</button>
       </header>
+      <ProductList />
     </div>
   );
 }
